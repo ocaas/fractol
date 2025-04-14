af1786a836ca215b1998fd86b86b65ae568f147d
olivia <oliviaacas@gmail.com>
2025-04-14 23:54:18 +0200

    seg fault

--- src/main.c
+++ src/main.c
@@ -6,29 +6,41 @@
 /*   By: olopez-s <olopez-s@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 01:36:59 by olopez-s          #+#    #+#             */
-/*   Updated: 2025/04/13 01:28:22 by olopez-s         ###   ########.fr       */
+/*   Updated: 2025/04/14 23:44:54 by olopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
 
-int main (void)
+#include "../includes/fractol.h"
+
+int	main(void)
 {
 	t_data	fractol;
 
 	fractol.mlx = mlx_init();
 	if (!fractol.mlx)
 		return (1);
-	
-	fractol.win = mlx_new_window(fractol.mlx, 800, 800, "window");
-	fractol.img = mlx_new_image(fractol.mlx, 600, 500);
-	fractol.addr = mlx_get_data_addr(fractol.mlx, &fractol.bpp, &fractol.line_len, &fractol.endian);
 
-	//missing something here
+	fractol.win = mlx_new_window(fractol.mlx, WIDTH, HEIGHT, "Fractal Window");
+	if (!fractol.win)
+		return (1);
+
+	fractol.img = mlx_new_image(fractol.mlx, WIDTH, HEIGHT);
+	if (!fractol.img)
+		return (1);
+
+	fractol.addr = mlx_get_data_addr(fractol.img, &fractol.bpp, &fractol.line_len, &fractol.endian);
+	if (!fractol.addr)
+		return (1);
+
 	ft_render(&fractol);
+
 	mlx_put_image_to_window(fractol.mlx, fractol.win, fractol.img, 0, 0);
-	mlx_key_hook(fractol.win, (int (*)())des_wind, &fractol);
+
+	mlx_key_hook(fractol.win, des_wind, &fractol);
 	mlx_loop(fractol.mlx);
 	return (0);
 }
+
